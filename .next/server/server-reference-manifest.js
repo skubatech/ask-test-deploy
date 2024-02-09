e5e8b03c356abdeb834f6aaeac1dbcb4cfa4ba48
kirillkarpakov <karpakovkirill@gmail.com>
2024-02-10 01:49:04 +0400

    fixes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"cBGiUU5v5owZiBCl8RNXS9tlaMyjLwYLG7JW1HKRupA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vW1sDGzDFdRH0wMiwFQkxhhSgpVazpJpNw3TTglQj2c=\"}"
