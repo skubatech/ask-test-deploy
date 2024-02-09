82b8a371e159f779ef6c4c7d3b404d88ddf6f018
kirillkarpakov <karpakovkirill@gmail.com>
2024-02-09 22:07:37 +0400

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import WP from './config/webpack.js'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     trailingSlash: true,
     assetPrefix: '/ask-test-deploy/',
     webpack: (config, { webpack }) => WP(config, webpack),
